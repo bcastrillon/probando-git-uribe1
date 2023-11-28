9ceea39452e239d9b46676f570fc3f902f707c43
Henry <john_henry.4m@hotmail.com>
2023-11-28 11:30:22 -0500

    se inicia proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Natillera abuela procesar
+        //que nececito para la natillera del cliente
+        //entradas
+
+        int identificadorCliente = 54656;
+        long documentoDeIdentidad = 3432423243L;
+        byte edadCliente = 34;
+        short cuotaMensual = 32000;
+        float tasaInteresFijo = 0.1F;
+        char rangoDliente = 'A' ;
+
+
+
+    }
+}
